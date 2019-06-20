9821a2ab6853eeffa5d67b84c63587fdb9832c1f
befecertified <deekor.baribefe@gmail.com>
2019-06-20 22:01:33 +0100

    Initial commit.

---
+++ test.js
@@ -0,0 +1,9 @@
+var message = "Node Js up and running on port 0000";
+
+console.log(message);
+sayHello("Befe");
+
+function sayHello(name) {
+  console.log(`Hello ${name}
+Welcome to my first nodejs app!`);
+}
